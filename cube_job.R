e7c67e178561851c202c40417a28d8e18e12aa51
Cervus1983 <cervus1983@gmail.com>
2017-06-25 12:58:28 +0200

    AWS SNS -> Nexmo

--- cube_job.R
+++ cube_job.R
@@ -1,10 +1,11 @@
 counter <- readRDS("counter.rds") + 1
 
 source("cube.R")
-message <- sprintf("%s %s %s, %s", cube[counter, 2], cube[counter, 1], cube[counter, 3], cube[counter, 4])
+source("../nexmo.R")
 
-topic <- readRDS("topic.rds")
-
-aws.sns::publish(topic = topic, message = message)
+send_text(
+	from = "Cube",
+	text = sprintf("%s %s %s, %s", cube[counter, 2], cube[counter, 1], cube[counter, 3], cube[counter, 4])
+)
 
 saveRDS(counter, "counter.rds")
